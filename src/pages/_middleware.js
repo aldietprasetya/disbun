99009b0e656c43d813772b88c43c0647e6184452
aldiet <110385207+aldiet@users.noreply.github.com>
2022-08-08 01:56:23 +0700

    update middleware

--- src/pages/_middleware.js
+++ src/pages/_middleware.js
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server"
 
 export default function middleware(req) {
     let cookie = req.cookies["next-auth.session-token"];
+    let sec_cookie = req.cookies["__Secure-next-auth.session-token"];
     const url = req.url
 
     // if (url != '/login' && !cookie) {
@@ -24,7 +25,7 @@ export default function middleware(req) {
       req.nextUrl.pathname == '/forgot-password'
     )
 
-    if (cookie) {
+    if (cookie || sec_cookie) {
       if (frontProtected) {
         const url2 = req.nextUrl.clone()
         url2.pathname = '/'
