c6543a5add056c70e57ddb9a188d06a55e168b66
aldiet <110385207+aldiet@users.noreply.github.com>
2022-10-06 14:14:45 +0700

    update env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const appConfig = {
   environment: process.env.NEXT_PUBLIC_ENVIRONMENT || 'development',
-  baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
+  baseUrl: process.env.NEXT_PUBLIC_BASE_URL + process.env.NEXT_PUBLIC_BASE_URL_VERSION,
 };
